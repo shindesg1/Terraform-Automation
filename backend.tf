63c4fa34df6baaffcb109802b95ac839ca1f9bb1
shindesg1 <122252101+shindesg1@users.noreply.github.com>
2023-05-03 23:39:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-bucket-s3-rahul-145"
     key = "main"
-    region = "ap-south-1"
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
